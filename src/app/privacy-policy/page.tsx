b051a82b8b8e0538f1ff8a392fcbfea40d34befd
jfelic <felicianojulian1@gmail.com>
2025-06-29 12:31:48 -0400

    Remove redundant use of Footer from privacy-policy

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Navbar } from '@/components/Navbar';
 import { ComingSoon } from '@/components/ui/coming-soon';   
-import { Footer } from '@/components/Footer';
 
 export default function PrivacyPolicy() {
     return (
         <div>
             <Navbar />
             <ComingSoon />
-            <Footer />
+            {/* No need to put Footer here because it is in root */}
         </div>
     );
 }
